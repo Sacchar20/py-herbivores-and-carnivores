4c1ffb7b0173d555016dab0ab8c87c1d6bc61113
Vladyslav <saccharcrpt@gmail.com>
2026-04-03 11:36:58 +0300

    Solution3

--- app/main.py
+++ app/main.py
@@ -1,15 +1,19 @@
 class Animal:
     alive: list["Animal"] = []
 
-    def __init__(self, name: str, health: int = 100) -> None:
+    def __init__(
+        self,
+        name: str,
+        health: int = 100
+    ) -> None:
         self.name: str = name
         self.health: int = health
         self.hidden: bool = False
         Animal.alive.append(self)
 
     def __repr__(self) -> str:
-        return (f"{{Name: {self.name},"
-                f" Health: {self.health}, Hidden: {self.hidden}}}")
+        return (f"{{Name: {self.name}, "
+                f"Health: {self.health}, Hidden: {self.hidden}}}")
 
 
 class Herbivore(Animal):
@@ -18,7 +22,10 @@ class Herbivore(Animal):
 
 
 class Carnivore(Animal):
-    def bite(self, beast: Animal) -> None:
+    def bite(
+        self,
+        beast: Animal
+    ) -> None:
         if isinstance(beast, Herbivore) and not beast.hidden:
             beast.health -= 50
             if beast.health <= 0 and beast in Animal.alive:
